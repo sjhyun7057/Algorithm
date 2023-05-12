5e06ba4a7a0d6d84bbb95928ec0ba024d6f5f98e
sjh <97644665+sjhyun7057@users.noreply.github.com>
2023-05-12 20:44:48 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,19 +1,18 @@
-
 def solution(survey, choices):
-    kakao_dict = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
-    score_dict = {1: 3, 2: 2, 3: 1, 4: 0, 5: 1, 6: 2, 7: 3}
-    for i in range(len(survey)):
-        if choices[i] > 4:
-            kakao_dict[survey[i][-1]] += score_dict[choices[i]]
-        elif choices[i] < 4:
-            kakao_dict[survey[i][0]] += score_dict[choices[i]]
     
-    kakao_key = list(kakao_dict.keys())
-    kakao_value = list(kakao_dict.values())
-    kakao_result = ''
-    for i in range(0, 8, 2):
-        if kakao_value[i] >= kakao_value[i+1]:
-            kakao_result += kakao_key[i]
+    ans_dict = {'RT':0, 'CF':0, 'JM':0, 'AN':0}
+    for i,j in zip(survey,choices):
+        if i in ans_dict.keys():
+            ans_dict[i] += j-4
         else:
-            kakao_result += kakao_key[i+1]
-    return kakao_result
+            ans_dict[''.join(sorted(i))] += 4-j
+    
+    answer = ''
+    print(ans_dict)
+    for k,v in ans_dict.items():
+        if v>0:
+            answer += k[1]
+        else :
+            answer += k[0] 
+    
+    return answer
