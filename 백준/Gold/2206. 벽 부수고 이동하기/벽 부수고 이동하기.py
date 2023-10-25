6732f0a271f6f59a19d609a161145c7cbb6e914a
sjh <97644665+sjhyun7057@users.noreply.github.com>
2023-10-25 22:44:00 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2044 ms, Memory: 303820 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,36 +1,47 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
+'''
+N x M 행렬
+0은 이동가능
+1은 이동 불가
 
+'''
+from pprint import pprint
 N, M = map(int, input().split())
-arr = [list(map(int, input().strip())) for _ in range(N)]
-dist_arr = [[[1000000,1000000] for i in range(M)] for _ in range(N)]
-dist_arr[0][0] = [1, 1]
-di = [1, -1, 0, 0]
-dj = [0, 0, 1, -1]
 
-def bfs(s):
-    q = deque()
-    q.append(s)
-    while q:
-        i, j, cnt = q.popleft()
-        for k in range(4):
-            ni = i + di[k]
-            nj = j + dj[k]
-            cnt_a = 0
-            if 0 <= ni < N and 0 <= nj < M:
-                if cnt == 1:
-                    if not arr[ni][nj] and dist_arr[ni][nj][1] > dist_arr[i][j][1] + 1:
-                        dist_arr[ni][nj][1] = dist_arr[i][j][1] + 1
-                        q.append((ni,nj,cnt))
+arr = [list(map(int, list(input()))) for _ in range(N)]
+visit_arr = [[[N*M + 1, N*M + 1]]*M for _ in range(N)]
+
+di, dj = [1,-1,0,0], [0,0,1,-1]
+
+start = [(N-1,M-1),0]
+from collections import deque
+q = deque()
+q.append(start)
+visit_arr[N-1][M-1] = [1,1]
+
+while q:
+    # print(q)
+    dot, crush = q.popleft()
+    x, y = dot
+    # pprint(visit_arr)
+    for k in range(4):
+        
+        ni = x+di[k]
+        nj = y+dj[k]
+        if 0<=ni<N and 0<=nj<M:
+            if crush:
+                if not arr[ni][nj] and visit_arr[ni][nj][1] > visit_arr[x][y][1] + 1:
+                    visit_arr[ni][nj] = [visit_arr[ni][nj][0], visit_arr[x][y][1] + 1]
+                    q.append([(ni,nj),crush])
+            else:
+                if arr[ni][nj]:
+                    visit_arr[ni][nj] = [visit_arr[ni][nj][0], visit_arr[x][y][0] + 1]
+                    q.append([(ni,nj),1])
                 else:
-                    if arr[ni][nj]:
-                        cnt_a = 1
-                    if dist_arr[ni][nj][cnt_a] > dist_arr[i][j][0] + 1:
-                        dist_arr[ni][nj][cnt_a] = dist_arr[i][j][0] + 1
-                        q.append((ni, nj, cnt_a))
-    if min(dist_arr[N-1][M-1]) == 1000000:
-        dist_arr[N-1][M-1] = [-1,-1]
-    return dist_arr[N-1][M-1]
+                    if visit_arr[ni][nj][0] > visit_arr[x][y][0] + 1:
+                        visit_arr[ni][nj] = [visit_arr[x][y][0] + 1,visit_arr[ni][nj][1]]
+                        q.append([(ni,nj),0])
 
-print(min(bfs((0,0,0))))
+if visit_arr[0][0][0] == N * M +1 and visit_arr[0][0][1] == N*M + 1:
+    print(-1)
+else:
+    print(min(visit_arr[0][0]))
