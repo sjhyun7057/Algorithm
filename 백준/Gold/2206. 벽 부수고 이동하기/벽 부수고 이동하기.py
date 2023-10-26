9ce5b53612547dc90d960a4fdc56533d4e1f1d55
sjh <97644665+sjhyun7057@users.noreply.github.com>
2023-10-26 23:36:53 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2536 ms, Memory: 68416 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,47 +1,55 @@
 '''
+q depth 계산하면서 하기 while q 안에 q만큼의 for문을 돌려서 길이를 depth +1 
 N x M 행렬
 0은 이동가능
 1은 이동 불가
 
 '''
+'''
+N x M 행렬
+0은 이동가능
+1은 이동 불가
+'''
+
 from pprint import pprint
 N, M = map(int, input().split())
 
 arr = [list(map(int, list(input()))) for _ in range(N)]
-visit_arr = [[[N*M + 1, N*M + 1]]*M for _ in range(N)]
+visit_arr = [[0]*M for _ in range(N)]
 
-di, dj = [1,-1,0,0], [0,0,1,-1]
+start = (0,0,0)
+def bfs(start):
+    from collections import deque
+    di, dj = [1,-1,0,0], [0,0,1,-1]
+    q = deque()
+    q.append(start)
+    visit_arr[0][0] = [1,0]
+    d = 0
+    while q:
+        d += 1
+        for _ in range(len(q)):
+            x, y, check = q.popleft()
+            if x == N-1 and y == M-1:
+                return d
+            for k in range(4):
+                ni = x+di[k]
+                nj = y+dj[k]
+                if 0<=ni<N and 0<=nj<M:
+                    if not check: #그전에 벽을 통과한적이 없을 때
+                        if arr[ni][nj]: # 벽을 만났을 때
+                            if not visit_arr[ni][nj]:
+                                visit_arr[ni][nj] = 2
+                                q.append((ni,nj,1))
+                        elif not arr[ni][nj]:# 길일때
+                            if visit_arr[ni][nj] != 1:
+                                visit_arr[ni][nj] = 1
+                                q.append((ni,nj,0))
+                    else: # 벽을 통과한 적이 있을 때 
+                        if not arr[ni][nj]: # 길만 고려
+                            if not visit_arr[ni][nj]:
+                                visit_arr[ni][nj] = 2
+                                q.append((ni,nj,1))
+    return -1
 
-start = [(N-1,M-1),0]
-from collections import deque
-q = deque()
-q.append(start)
-visit_arr[N-1][M-1] = [1,1]
 
-while q:
-    # print(q)
-    dot, crush = q.popleft()
-    x, y = dot
-    # pprint(visit_arr)
-    for k in range(4):
-        
-        ni = x+di[k]
-        nj = y+dj[k]
-        if 0<=ni<N and 0<=nj<M:
-            if crush:
-                if not arr[ni][nj] and visit_arr[ni][nj][1] > visit_arr[x][y][1] + 1:
-                    visit_arr[ni][nj] = [visit_arr[ni][nj][0], visit_arr[x][y][1] + 1]
-                    q.append([(ni,nj),crush])
-            else:
-                if arr[ni][nj]:
-                    visit_arr[ni][nj] = [visit_arr[ni][nj][0], visit_arr[x][y][0] + 1]
-                    q.append([(ni,nj),1])
-                else:
-                    if visit_arr[ni][nj][0] > visit_arr[x][y][0] + 1:
-                        visit_arr[ni][nj] = [visit_arr[x][y][0] + 1,visit_arr[ni][nj][1]]
-                        q.append([(ni,nj),0])
-
-if visit_arr[0][0][0] == N * M +1 and visit_arr[0][0][1] == N*M + 1:
-    print(-1)
-else:
-    print(min(visit_arr[0][0]))
+print(bfs(start))
