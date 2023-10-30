959c8ddd5563610e498b5af13bca2a57647295ae
sjh <97644665+sjhyun7057@users.noreply.github.com>
2023-10-30 22:03:06 +0900

    [Silver IV] Title: 상근이의 여행, Time: 156 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,7 @@
+import sys 
+input = sys.stdin.readline
+for ts in range(int(input())):
+    N, M  = map(int, input().split())
+    for _ in range(M):
+        a,b = map(int,input().split())
+    print(N-1)
